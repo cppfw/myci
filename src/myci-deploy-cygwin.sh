8df4d4e053b68b94db0013806968ddf9ac3efe4a
Ivan Gagis <igagis@gmail.com>
2017-11-29 14:38:00 +0200

    release 0.1.4

--- src/myci-deploy-cygwin.sh
+++ src/myci-deploy-cygwin.sh
@@ -104,7 +104,7 @@ done
 cd $repodir
 
 	#run mksetupini
-	mksetupini --arch $architecture --inifile=$architecture/setup.ini --releasearea=. --okmissing=required-package --okmissing=curr
+	mksetupini --arch $architecture --inifile=$architecture/setup.ini --releasearea=. --okmissing=required-package
 	[ $? -ne 0 ] && source myci-error.sh "'mksetupini' failed";
 
 	bzip2 <$architecture/setup.ini >$architecture/setup.bz2
