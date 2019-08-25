3ff8516379556af34f800656cae7c5ac736e3ade
Ivan Gagis <igagis@gmail.com>
2019-08-26 00:29:03 +0300

    testing coloring

--- src/myci-passed.sh
+++ src/myci-passed.sh
@@ -3,4 +3,4 @@
 #we want exit immediately if any command fails and we want error in piped commands to be preserved
 set -eo pipefail
 
-test -t 1 && printf "\t\\033[1;32mPASSED\\033[0m\n" || printf "\tPASSED\n"
+test -t 1 && printf "\t\e[1;32mPASSED\e[0m\n" || printf "\tPASSED\n"
