0091184374e27493f04a53e8381061095f42abf3
Ivan Gagis <igagis@gmail.com>
2019-11-27 00:21:12 +0200

    test logging

--- tests/apply-version/test.sh
+++ tests/apply-version/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#we want exit immediately if any command fails and we want error in piped commands to be preserved
+# we want exit immediately if any command fails and we want error in piped commands to be preserved
 set -eo pipefail
 
 testname=$(pwd)
@@ -9,10 +9,15 @@ testname=${testname##*/}
 
 rm -f *.txt
 ../../src/myci-apply-version.sh -v 1.2.3 *.in
-if [ ! -f test-1.2.3.txt ]; then ../../src/myci-error.sh "test-1.2.3.txt file not found"; fi
-cmp test-1.2.3.txt test.smp || ../../src/myci-error.sh "test-1.2.3.txt contents are not as expected";
+if [ ! -f test-1.2.3.txt ]; then
+	../../src/myci-error.sh "test-1.2.3.txt file not found";
+fi
+cmp test-1.2.3.txt test.smp || (echo "test-1.2.3.txt =" && xxd test-1.2.3.txt && echo "test.smp =" && xxd test.smp && ../../src/myci-error.sh "test-1.2.3.txt contents are not as expected");
+
 rm -f *.txt
 ../../src/myci-apply-version.sh -v 1.2.3 *.in --filename-only
-if [ ! -f test-1.2.3.txt ]; then ../../src/myci-error.sh "test-1.2.3.txt file not found"; fi
+if [ ! -f test-1.2.3.txt ]; then
+	../../src/myci-error.sh "test-1.2.3.txt file not found";
+fi
 cmp test-1.2.3.txt test-\$\(version\).txt.in || ../../src/myci-error.sh "test-1.2.3.txt contents are not as expected (test-\$(version).txt.in)";
 ../../src/myci-passed.sh
