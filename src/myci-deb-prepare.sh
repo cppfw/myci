ea1bcf2818399219cd4268b253573418d7cef61b
Ivan Gagis <igagis@gmail.com>
2020-06-14 00:51:45 +0300

    allow specifying soname from command line  to myci-deb-prepare.sh script

--- src/myci-deb-prepare.sh
+++ src/myci-deb-prepare.sh
@@ -5,10 +5,41 @@ set -eo pipefail
 
 # This script is used for preparing the debian package before building with dpkg-buildpackage.
 
+while [[ $# > 0 ]] ; do
+    case $1 in
+        --help)
+            echo "Usage:"
+            echo "	$(basename $0) [-s <soname> -d <path-to-debianization-dir>]"
+            echo " "
+            echo "If '-s' parameter is not given the script tries to read soname from src/soname.txt file."
+			echo "If '-d' parameter is not given the script tries to locate debianization files in 'debian' directory."
+            echo " "
+            echo "Example:"
+            echo "	$(basename $0) -s 4 -d debian"
+            exit 0
+			;;
+        -s)
+			shift
+			soname=$1
+			;;
+		-d)
+			shift
+			debianization=$1
+			;;
+    esac
+
+	[[ $# > 0 ]] && shift;
+done
 
 echo "Preparing Debian package for building"
 
-soname=$(cat src/soname.txt 2>/dev/null || true) #ignore error if there is no soname.txt file
+if [ -z "$debianization" ]; then
+	debianization=debian
+fi
+
+if [ -z "$soname" ]; then
+	soname=$(cat src/soname.txt 2>/dev/null || true) # ignore error if there is no soname.txt file
+fi
 
 if [ -z "$soname" ]; then
 	echo "no soname found, using empty soname"
@@ -17,15 +48,15 @@ else
 	echo "Detected soname = $soname"
 fi
 
-listOfInstalls=$(ls debian/*.install.in 2>/dev/null || true) #allow package without *.install.in
+listOfInstalls=$(ls $debianization/*.install.in 2>/dev/null || true) #allow package without *.install.in
 
 for i in $listOfInstalls; do
 	echo "Applying soname to $i"
 	cp $i ${i%.install.in}$soname.install
 done
 
-echo "Applying soname to debian/control.in"
+echo "Applying soname to $debianization/control.in"
 
-sed -e "s/\$(soname)/$soname/g" debian/control.in > debian/control
+sed -e "s/\$(soname)/$soname/g" $debianization/control.in > $debianization/control
 
 echo "Debian package prepared for building!"
