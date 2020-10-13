bcd0a7f5c5043632e36c41c339bc73ab2d6c1303
Ivan Gagis <igagis@gmail.com>
2020-10-13 16:59:19 +0300

    create-zip-package script: allow placing header files to custom subdirectory within include directory of the package

--- src/myci-create-zip-package.sh
+++ src/myci-create-zip-package.sh
@@ -9,11 +9,12 @@ while [[ $# > 0 ]] ; do
 		--help)
 			echo "Script for packing the library to ZIP package."
 			echo "Usage:"
-			echo "	$(basename $0) -h <headers-dir> [-f <source-filename>///<destination-directory-within-archive> ...] <output-package-filename>"
+			echo "	$(basename $0) -h <headers-dir>[///<headers-dest-subdir>] [-f <source-filename>[///<destination-directory-within-archive>] ...] <output-package-filename>"
 			echo " "
 			echo "The script packs supplied headers (.hpp and .h) preserving directory structure into the 'include' directory withing archive."
 			echo "There can be any number of -f keys. The '///<destination-directory-within-archive>' part of the -f key value can be omitted,"
 			echo "the file is then added to the root directory within the archive."
+			echo "The '///<headers-dest-subdir>' part of the -h key can be omitted, then header files are copied to 'include' directory of the archive."
 			echo " "
 			echo "Example:"
 			echo "	$(basename $0) -h ./src -f xcode/build/libsomething.a///lib/ios/ -f licenses/MyLICENSE.txt libsomething.zip"
@@ -38,8 +39,19 @@ done
 
 [ -z "$hdrdir" ] && source myci-error.sh "headers directory is not given";
 
-# make sure header dir ends with '/' (remove '/' suffix if it is there and then add '/' again)
-hdrdir="${hdrdir%/}/"
+# split header files source and destination directories
+hdrdir_srcdst=(${hdrdir//\/\/\// })
+
+# make sure header dirs end with '/' (remove '/' suffix if it is there and then add '/' again)
+hdrdir="${hdrdir_srcdst[0]%/}/"
+
+hdrdir_dst=${hdrdir_srcdst[1]}
+if [ ! -z "$hdrdir_dst" ]; then
+	hdrdir_dst="${hdrdir_dst%/}/"
+fi
+
+echo "hdrdir=$hdrdir"
+echo "hdrdir_dst=$hdrdir_dst"
 
 echo "Creating ZIP package"
 
@@ -54,7 +66,7 @@ tmp_dir=$(mktemp -d -t myci-XXXXXXXXXXXXXXXXX)
 
 for header in $headers; do
 	f=${header#"$hdrdir"}
-	dstf="$tmp_dir/include/$f"
+	dstf="$tmp_dir/include/$hdrdir_dst$f"
 	dstdir=$(dirname $dstf)
 	mkdir -p $dstdir
 	cp $header $dstdir
