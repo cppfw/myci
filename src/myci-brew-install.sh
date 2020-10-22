a5c95f2602027b4bf30ec6e958ef209714782e9c
Ivan Gagis <ivan.gagis@korulab.com>
2020-10-22 21:12:29 +0300

    brew install script

---
+++ src/myci-brew-install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# we want exit immediately if any command fails and we want error in piped commands to be preserved
+set -eo pipefail
+
+# Script for installing or upgrading homebrew packages
+
+while [[ $# > 0 ]] ; do
+	case $1 in
+		--help)
+			echo "Usage:"
+			echo "	$(basename $0) [<package-name> ...]"
+			echo " "
+			echo "Install or upgrade homebrew packages."
+			echo " "
+			echo "Example:"
+			echo "	$(basename $0) liba libb"
+			exit 0
+			;;
+		*)
+			inputs="$inputs $1"
+			shift
+			;;
+	esac
+done
+
+[ -z "$inputs" ] && echo "Error: no packages to install specified" && exit 1;
+
+# echo inputs = $inputs
+
+for i in $inputs; do
+echo $i
+    # check if package is already installed
+    set +e
+    brew list $i > /dev/null 2>&1
+    exit_code=$?
+    set -e
+    if [ $exit_code != 0 ]; then
+        echo "no package '$i' installed, install it";
+        brew install $i;
+    else
+        echo "package '$i' is already installed, upgrade it";
+        brew upgrade $i;
+    fi;
+done
