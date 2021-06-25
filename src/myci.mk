4128cf034625e39f2cab127bab90a67abc322491
Ivan Gagis <igagis@gmail.com>
2021-06-25 16:18:48 +0300

    deprecate myci-try-include

--- src/myci.mk
+++ src/myci.mk
@@ -4,6 +4,7 @@ ifneq ($(myci_is_included),true)
 
     # include makefile if it is not included already, does not fail if file does not exist
     define myci-try-include
+        $(info DEPRECATED: myci-try-include: use prorab-try-simple-include)
         $(eval myci_private_include_file := $(abspath $1))
         $(if $(filter $(myci_private_include_file),$(MAKEFILE_LIST)), \
                 , \
