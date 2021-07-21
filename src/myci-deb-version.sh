9958f4a384af338f10ec8193eb30bb1ae5a8b8cc
Ivan Gagis <igagis@gmail.com>
2021-07-21 13:29:56 +0300

    improve myci-deb-version script

--- src/myci-deb-version.sh
+++ src/myci-deb-version.sh
@@ -3,4 +3,18 @@
 # we want exit immediately if any command fails and we want error in piped commands to be preserved
 set -eo pipefail
 
-head -1 $1 | sed -n -e 's/.*(\([\.0-9]*\)\(-[0-9]*\)\{0,1\}).*/\1/p'
+script_dir="$(dirname $0)/"
+source ${script_dir}myci-common.sh
+
+changelog_file=$1
+
+if [ -z "$changelog_file" ]; then
+    echo "no input changelog file given, trying to use debian/changelog"
+    changelog_file="debian/changelog"
+fi
+
+if [ ! -f "$changelog_file" ]; then
+    error "no '$changelog_file' found"
+fi
+
+head -1 $changelog_file | sed -n -e 's/.*(\([\.0-9]*\)\(-[0-9]*\)\{0,1\}).*/\1/p'
