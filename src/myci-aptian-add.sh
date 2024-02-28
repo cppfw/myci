3bbd04bb11a42cfdc2e4204c5cff4f2e0ecd7dce
Ivan Gagis <ivan.gagis@ge.com>
2024-02-28 13:21:48 +0200

    aptian

---
+++ src/myci-aptian-add.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# we want exit immediately if any command fails and we want error in piped commands to be preserved
+set -eo pipefail
+
+script_dir="$(dirname $0)/"
+source ${script_dir}myci-common.sh
+
+while [[ $# > 0 ]] ; do
+	case $1 in
+		--help)
+			echo "usage:"
+			echo "	$(basename $0) [--help] <options> <deb-file> [<deb-file>...]"
+			echo " "
+			echo "options:"
+            echo "  --help                   show this help text and do nothing."
+			echo "  --base-dir <base-dir>    required option, base directory where all repos are stored."
+			echo "  --repo <repo>            required option, repository name, e.g. 'cppfw/debian'."
+			echo "  --distro <distro>        required option, debian repo distro, e.g. 'buster'."
+            echo "  --component <component>  required option, debian repo component, e.g. 'main'."
+			exit 0
+			;;
+		--base-dir)
+			shift
+			base_dir=$1/
+            [ -d "$base_dir" ] || error "base directory '$base_dir' does not exist"
+			;;
+		--owner)
+			echo "DEPRECATED: --owner, use --repo <owner>/<repo-name> instead"
+			shift
+			owner=$1
+			;;
+		--repo)
+            shift
+            repo=$1
+            ;;
+		--distro)
+			shift
+			distro=$1
+			;;
+		--component)
+			shift
+			component=$1
+			;;
+		*)
+            files="$files $1"
+			;;
+	esac
+	[[ $# > 0 ]] && shift;
+done
+
+[ ! -z "$base_dir" ] || error "missing required argument: --base-dir"
+[ ! -z "$repo" ] || error "missing required argument: --repo"
+[ ! -z "$distro" ] || error "missing required option: --distro"
+[ ! -z "$component" ] || error "missing required argument: --component"
+[ ! -z "$files" ] || error "missing deb files to add"
+
+repo_dir="$(realpath --canonicalize-missing ${base_dir}${owner}/${repo})/"
+
+# create repo dir if needed
+
+if [ ! -d "$repo_dir" ]; then
+	mkdir -p $repo_dir
+fi
+
+function perform_aptian_add {
+	local conf_file="${repo_dir}etc/freight.conf"
+
+	if [ ! -f "${conf_file}" ]; then
+		first_key_email=$(gpg --list-keys | sed -E -n -e 's/.*<([^ >]*)>.*/\1/p' | head -1)
+		[ ! -z "$first_key_email" ] || error "no default GPG key found"
+		# freight-init --gpg=$first_key_email --libdir=${repo_dir}lib --cachedir=${repo_dir} --archs="source" ${repo_dir}
+        aptian init --gpg=$first_key_email --dir=${repo_dir}
+	fi
+
+    aptian add --dir=${repo_dir} --dist=$distro --comp=$component $files
+}
+
+(
+	# 1 hour timeout = 3600 seconds
+	flock --verbose --exclusive --timeout 3600 200
+    
+	perform_aptian_add
+) 200>${repo_dir}lock
---
+++ src/myci-deploy-apt-ssh.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# we want exit immediately if any command fails and we want error in piped commands to be preserved
+set -eo pipefail
+
+script_dir="$(dirname $0)/"
+
+source ${script_dir}myci-common.sh
+
+user=repo
+base_dir=/var/www/repo/
+
+while [[ $# > 0 ]] ; do
+	case $1 in
+		--help)
+			echo "usage:"
+			echo "	$(basename $0) [--help] <options> <deb-file> [<deb-file>...]"
+			echo " "
+			echo "options:"
+            echo "  --help                   show this help text and do nothing."
+            echo "  --key <ssh-key>          ssh private key to authenticate to the server."
+			echo "  --server <server>        ssh server."
+            echo "  --user <user>            linux user name on the server, defaults to 'repo'."
+            echo "  --base-dir <path>        repositories base directory on the server, defaults to 'repo'."
+            echo "  --repo <repo>            debian repo name, e.g. 'cppfw/debian'."
+            echo "  --distro <distro>        debian distro name, e.g. 'bookworm'."
+            echo "  --component <component>  debian repo component, e.g. 'main'."
+			exit 0
+			;;
+        --key)
+            shift
+            ssh_key=$1
+            ;;
+        --server)
+            shift
+            server=$1
+            ;;
+        --user)
+            shift
+            user=$1
+            ;;
+		--base-dir)
+			shift
+			base_dir=$1/
+			;;
+		--owner)
+			echo "DEPRECATED: --owner, use --repo <owner>/<repo-name> instead"
+			shift
+			owner=$1
+			;;
+		--repo)
+            shift
+            repo=$1
+            ;;
+        --distro)
+            shift
+            distro=$1
+            ;;
+        --component)
+            shift
+            component=$1
+            ;;
+		*)
+            files="$files $1"
+			;;
+	esac
+	[[ $# > 0 ]] && shift;
+done
+
+[ ! -z "$server" ] || error "required option is not given: --server"
+[ ! -z "$ssh_key" ] || error "required option is not given: --key"
+[ ! -z "$repo" ] || error "required option is not given: --repo"
+[ ! -z "$distro" ] || error "required option is not given: --distro"
+[ ! -z "$component" ] || error "required option is not given: --component"
+[ ! -z "$files" ] || error "no package files given"
+
+# TODO: --owner is DEPRECATED, remove when support for --owner is removed
+if [ ! -z "$owner" ]; then
+    owner="${owner}/"
+fi
+
+ssh_opts="-i ${ssh_key} -o IdentitiesOnly=yes -o StrictHostKeyChecking=no"
+
+tmp_dir=$(ssh ${ssh_opts} $user@$server mktemp --tmpdir --directory myci_upload.XXXXXXXXXX)
+# echo "tmp_dir = $tmp_dir"
+trap "ssh ${ssh_opts} $user@$server rm -rf $tmp_dir" EXIT ERR
+
+scp ${ssh_opts} $files $user@$server:$tmp_dir
+
+remote_files=$(ssh ${ssh_opts} $user@$server ls -d $tmp_dir/*)
+# echo "remote_files = $remote_files"
+
+ssh ${ssh_opts} $user@$server myci-aptian-add.sh --base-dir $base_dir --repo ${owner}${repo} --distro $distro --component $component $remote_files
