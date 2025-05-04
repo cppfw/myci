0a89df182d4c931948f73be5c51d854ac94cf22c
Ivan Gagis <igagis@gmail.com>
2025-05-04 18:41:57 +0300

    improve test

--- tests/apply-version/test.sh
+++ tests/apply-version/test.sh
@@ -21,12 +21,12 @@ if [ ! -f test-1.2.3.txt ]; then
 fi
 cmp test-1.2.3.txt test-\$\(version\).txt.in || ../../src/myci-error.sh "test-1.2.3.txt contents are not as expected (test-\$(version).txt.in)";
 
+rm -rf out
 mkdir -p out
 ../../src/myci-apply-version.sh --version 1.2.3 ./*.in --out-dir out/
 if [ ! -f out/test-1.2.3.txt ]; then
 	../../src/myci-error.sh "out/test-1.2.3.txt file not found";
 fi
 cmp out/test-1.2.3.txt test.smp || ../../src/myci-error.sh "out/test-1.2.3.txt contents are not as expected (test-\$(version).txt.in)";
-rm -rf out
 
 ../../src/myci-passed.sh
