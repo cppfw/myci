bce20844e19a920cd09215fc0a617ec14a5a85c6
Ivan Gagis <igagis@gmail.com>
2025-05-24 19:06:37 +0300

    powershell: improve script

--- src/powershell/myci-deb-version.ps1
+++ src/powershell/myci-deb-version.ps1
@@ -8,6 +8,9 @@ $scriptdir = Split-Path $MyInvocation.MyCommand.Path
 
 If(!$debdir){
     $debdir = "build/debian/"
+    if(!(Test-Path -Path $debdir)){
+        $debdir = "debian/"
+    }
 }
 
 $ver = (Get-Content $debdir/changelog -Head 1) -replace ".*\((\d*\.\d*\.\d*)(\-\d+){0,1}\).*",'$1'
